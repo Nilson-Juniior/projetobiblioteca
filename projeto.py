ea05944af1140c7380d175e9b01c343917131e25
richaferreira <127885121+richaferreira@users.noreply.github.com>
2024-06-03 21:07:29 -0300

    Update 2 projeto.py

--- projeto.py
+++ projeto.py
@@ -4,12 +4,22 @@ class Usuario:
         self.password = senha
 
 class Funcionario(Usuario):
-    def __init__(self, usuario, senha, nome):
+    def __init__(self, usuario, senha, nome, cargo):
         super().__init__(usuario, senha)
         self.nome = nome
+        self.cargo = cargo
 
     def __str__(self):
-        return f"Nome: {self.nome}, usuário: {self.username}"
+        return f"Nome: {self.nome}, Usuario: {self.username}, Cargo: {self.cargo}"
+
+class Cliente(Usuario):
+    def __init__(self, usuario, senha, nome, email):
+        super().__init__(usuario, senha)
+        self.nome = nome
+        self.email = email
+
+    def __str__(self):
+        return f"Nome: {self.nome}, Email: {self.email}"
 
 class Livro:
     def __init__(self, titulo, autor, ano_publicacao):
@@ -20,87 +30,121 @@ class Livro:
     def __str__(self):
         return f"Título: {self.titulo}, Autor: {self.autor}, Ano de Publicação: {self.ano_publicacao}"
 
-def adicionar_livro(livros):
+def importar_usuarios():
+    usuarios = []
     try:
+        with open("usuarios.txt", "r") as arquivo:
+            linhas = arquivo.readlines()
+            for linha in linhas:
+                dados = linha.strip().split(",")
+                if dados[3] == "Funcionario":
+                    usuario = Funcionario(dados[0], dados[1], dados[2], dados[3])
+                else:
+                    usuario = Cliente(dados[0], dados[1], dados[2], dados[3])
+                usuarios.append(usuario)
+        print("Usuários importados com sucesso!\n")
+    except FileNotFoundError:
+        print("Arquivo de usuários não encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao importar usuários: {e}")
+    return usuarios
+
+def importar_livros():
+    livros = []
+    try:
+        with open("livros.txt", "r") as arquivo:
+            linhas = arquivo.readlines()
+            for linha in linhas:
+                dados = linha.strip().split(",")
+                livro = Livro(dados[0], dados[1], dados[2])
+                livros.append(livro)
+        print("Livros importados com sucesso!\n")
+    except FileNotFoundError:
+        print("Arquivo de livros não encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao importar livros: {e}")
+    return livros
+
+def cadastrar_usuario(usuarios):
+    username = input("Digite o nome de usuário: ")
+    senha = input("Digite a senha: ")
+    nome = input("Digite o nome do usuário: ")
+    cargo = input("Digite o cargo (Funcionario/Admin): ")
+    if cargo.lower() == "admin":
+        senha_admin = input("Digite a senha de administrador: ")
+        if senha_admin == "admin123":  # Verifica se a senha de administrador está correta
+            usuario = Funcionario(username, senha, nome, cargo)
+            usuarios.append(usuario)
+            print("Usuário cadastrado com sucesso!\n")
+        else:
+            print("Senha de administrador incorreta. Não é possível cadastrar um administrador.\n")
+    else:
+        usuario = Funcionario(username, senha, nome, cargo)
+        usuarios.append(usuario)
+        print("Usuário cadastrado com sucesso!\n")
+
+def login(usuarios):
+    username = input("Digite o nome de usuário: ")
+    senha = input("Digite a senha: ")
+
+    for usuario in usuarios:
+        if usuario.username == username and usuario.password == senha:
+            print("Login bem-sucedido!\n")
+            return usuario
+
+    print("Nome de usuário ou senha incorretos.\n")
+    return None
+
+def adicionar_livro(livros, usuario_logado):
+    if isinstance(usuario_logado, Funcionario) and usuario_logado.cargo == "Admin":
         titulo = input("Digite o título do livro: ")
         autor = input("Digite o autor do livro: ")
         ano_publicacao = input("Digite o ano de publicação do livro: ")
 
-        if not titulo or not autor or not ano_publicacao:
-            raise ValueError("Todos os campos devem ser preenchidos.")
-
         livro = Livro(titulo, autor, ano_publicacao)
         livros.append(livro)
+
         print("Livro adicionado com sucesso!\n")
-    except ValueError as e:
-        print(f"Erro ao adicionar livro: {e}\n")
+    else:
+        print("Acesso negado. Somente administradores podem adicionar livros.\n")
 
 def visualizar_livros(livros):
-    try:
-        if livros:
-            print("Lista de livros cadastrados:\n")
-            for livro in livros:
-                print(livro)
-        else:
-            print("Nenhum livro cadastrado.\n")
-    except Exception as e:
-        print(f"Erro ao visualizar livros: {e}\n")
-
-def adicionar_funcionario(funcionarios):
-    try:
-        login = input("Digite o login do funcionário: ")
-        senha = input("Digite a senha do funcionário: ")
-        nome = input("Digite o nome do funcionário: ")
-
-        if not login or not senha or not nome:
-            raise ValueError("Todos os campos devem ser preenchidos.")
-
-        funcionario = Funcionario(login, senha, nome)
-        funcionarios.append(funcionario)
-        print("Funcionário adicionado com sucesso!\n")
-    except ValueError as e:
-        print(f"Erro ao adicionar funcionário: {e}\n")
-
-def visualizar_funcionarios(funcionarios):
-    try:
-        if funcionarios:
-            print("Lista de funcionários cadastrados:\n")
-            for funcionario in funcionarios:
-                print(funcionario)
-        else:
-            print("Nenhum funcionário cadastrado.\n")
-    except Exception as e:
-        print(f"Erro ao visualizar funcionários: {e}\n")
+    if livros:
+        print("Lista de livros cadastrados:\n")
+        for livro in livros:
+            print(livro)
+    else:
+        print("Nenhum livro cadastrado.\n")
 
 def main():
-    funcionarios = []
-    livros = []
+    usuarios = importar_usuarios()
+    clientes = []
+    livros = importar_livros()
 
     while True:
-        try:
-            print("Menu Principal")
-            print("1. Adicionar Livro")
-            print("2. Visualizar Livros")
-            print("3. Cadastrar Funcionário")
-            print("4. Visualizar Funcionários")
-            print("5. Sair")
-            opcao = input("Escolha uma opção: ")
+        print("Menu Principal")
+        print("1. Cadastrar Usuário")
+        print("2. Login")
+        print("3. Adicionar Livro")
+        print("4. Visualizar Livros")
+        print("5. Sair")
+        opcao = input("Escolha uma opção: ")
 
-            if opcao == '1':
-                adicionar_livro(livros)
-            elif opcao == '2':
-                visualizar_livros(livros)
-            elif opcao == '3':
-                adicionar_funcionario(funcionarios)
-            elif opcao == '4':
-                visualizar_funcionarios(funcionarios)
-            elif opcao == '5':
-                print("Saindo do sistema.")
-                break
-            else:
-                print("Opção inválida. Tente novamente.\n")
-        except Exception as e:
-            print(f"Ocorreu um erro no sistema: {e}\n")
+        if opcao == '1':
+            cadastrar_usuario(usuarios)
+        elif opcao == '2':
+            usuario_logado = login(usuarios + clientes)
+            if usuario_logado:
+                print(f"Bem-vindo, {usuario_logado.nome}!\n")
+        elif opcao == '3':
+            adicionar_livro(livros, usuario_logado)
+        elif opcao == '4':
+            visualizar_livros(livros)
+        elif opcao == '5':
+            print("Saindo do sistema.")
+            break
+        else:
+            print("Opção inválida. Tente novamente.\n")
 
 if __name__ == "__main__":
     main()
